bf620e1860f5928a70989eb39e3c1d95a16b9a1b
narmatov0410 <almazbeknarmatov6@gmail.com>
2023-11-25 16:29:10 +0500

    yangiliklar qoshildi

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -3,5 +3,7 @@ package org.example;
 public class Task {
     public void task1() {
         int a = 1;
+        int b = 2;
+
     }
 }
--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,8 +1,7 @@
 package org.example;
 
-public class Main {
+public class Task2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        System.out.println("new task");
     }
 }
